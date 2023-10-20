1d57919174258c14ae1cc85baaecb572ef32814f
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2023-10-20 12:27:46 +0300

    Delete homeWork2/ConcreteComponent.java

--- homeWork2/ConcreteComponent.java
+++
@@ -1,8 +0,0 @@
-package homeWork2;
-
-public class ConcreteComponent implements Component {
-    public void operation() {
-        System.out.println("Performing operation in ConcreteComponent");
-    }
-}
-
