2c97fd8c6bb33f5143a9b1188ccdad60c2b81bd5
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2023-11-14 11:39:39 +0300

    Add files via upload

---
+++ Seminar9/MyFirstWebApplication/MyFirstWebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApplication.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyFirstWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private WeatherForecastHolder _weatherForecastHolder;
+
+        public WeatherForecastController(
+            WeatherForecastHolder weatherForecastHolder)
+        {
+            _weatherForecastHolder = weatherForecastHolder;
+        }
+
+
+        [HttpPost("add-data")]
+        public ActionResult<bool> Add([FromQuery] DateTime date, [FromQuery] int temperatureC)
+        {
+            bool result = _weatherForecastHolder.Add(date, temperatureC);
+            return Ok(result);
+        }
+
+        [HttpPut("update-data")]
+        public ActionResult<bool> Update([FromQuery] DateTime date, [FromQuery] int temperatureC)
+        {
+            bool result = _weatherForecastHolder.Update(date, temperatureC);
+            if (result)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound(); // Возвращаем NotFound в случае, если обновление не удалось найти запись для обновления
+            }
+        }
+
+        [HttpDelete("delete-data")]
+        public IActionResult Delete([FromQuery] DateTime date)
+        {
+            bool result = _weatherForecastHolder.Delete(date);
+            if (result)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound(); // Возвращаем NotFound, если запись с указанной датой не была найдена
+            }
+        }
+
+
+        [HttpGet("get-data")]
+        public ActionResult<List<WeatherForecast>> Get([FromQuery] DateTime from, [FromQuery] DateTime to)
+        {
+            List<WeatherForecast> result = _weatherForecastHolder.Get(from, to);
+            return Ok(result);
+        }
+
+    }
+}
